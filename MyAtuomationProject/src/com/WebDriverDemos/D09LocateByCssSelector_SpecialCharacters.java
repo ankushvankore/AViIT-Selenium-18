c1b46675a7b8d37b4494faeab0cad6f77426830d
Ankush <ankushvankore@gmail.com>
2026-03-07 09:53:25 +0530

    Date: 07 Mar 2026

---
+++ MyAtuomationProject/src/com/WebDriverDemos/D09LocateByCssSelector_SpecialCharacters.java
@@ -0,0 +1,33 @@
+package com.WebDriverDemos;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class D09LocateByCssSelector_SpecialCharacters {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://register.rediff.com/register/register.php?FormName=user_details");
+		
+		//^ - Starts with
+		WebElement fullName = driver.findElement(By.cssSelector("input[name^='name']"));
+		fullName.sendKeys("Dhanashri Thorat");
+		WebElement rediffId = driver.findElement(By.cssSelector("input[name^='login']"));
+		rediffId.sendKeys("dhanashri");
+		
+		//$ - Ends With
+		WebElement password = driver.findElement(By.cssSelector("input[placeholder$='password'][name^='pass']"));
+		password.sendKeys("dhanashri@123");
+		
+		//* - Contains
+		WebElement retypePassword = driver.findElement(By.cssSelector("input[placeholder*='type']"));
+		retypePassword.sendKeys("dhanashri@123");
+		
+		
+	}
+
+}
