40959543dcc8810f77401386782c1cae70bfd578
Ankush <ankushvankore@gmail.com>
2026-03-26 08:36:21 +0530

    Date: 25 Mar 2026

---
+++ MyAtuomationProject/src/com/TestNGDemo/D01FirstTest.java
@@ -0,0 +1,21 @@
+package com.TestNGDemo;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class D01FirstTest {
+	@Test
+	public void testRediff() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://www.rediff.com/");
+		System.out.println("Title: " + driver.getTitle());
+		
+		driver.close();
+	}
+}
