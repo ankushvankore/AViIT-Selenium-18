fb0a5259a43925eafb5278c5c17a9b505ff191f0
Ankush <ankushvankore@gmail.com>
2026-04-09 19:42:40 +0530

    Date: 09 Apr 2026

--- MyAtuomationProject/src/com/TestNGDemos/D10POM_Client.java
+++ MyAtuomationProject/src/com/TestNGDemos/D10POM_Client.java
@@ -1,12 +1,15 @@
 package com.TestNGDemos;
 
 import org.testng.annotations.Test;
+
+
 import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 
@@ -15,21 +18,25 @@ public class D10POM_Client {
 	RediffUtility r1;
 	
 	@Test
-	public void registration1() {
+	public void registration1() throws InterruptedException {
 		r1.enterFullName("Dhanashri Thorat");
-		r1.enterRediffId("dhanashrithorat");
+		r1.enterRediffId("dhanashri_thorat");
 		r1.enterPassword("Dhanashri@123");
 		r1.setBirthDate("30", "MAR", "2000");
-		r1.selectGender("Female");		
+		r1.selectGender("Female");	
+		String msg = r1.checkAvailablity();
+		Assert.assertTrue(msg.contains("Yippie"));
 	}
 	
 	@Test
-	public void registration2() {
+	public void registration2() throws InterruptedException {
 		r1.enterFullName("Ravi Shinde");
 		r1.enterRediffId("ravishinde");
 		r1.enterPassword("Ravi@123");
 		r1.setBirthDate("01", "JUN", "1979");
-		r1.selectGender("Male");		
+		r1.selectGender("Male");
+		//r1.checkAvailablity();
+		Assert.assertTrue(r1.checkAvailablity().contains("Yippie"));
 	}
 	
 	@AfterMethod
@@ -51,6 +58,7 @@ public class D10POM_Client {
 
 	@AfterTest
 	public void afterTest() {
+		driver.close();
 	}
 
 }
--- MyAtuomationProject/src/com/TestNGDemos/RediffUtility.java
+++ MyAtuomationProject/src/com/TestNGDemos/RediffUtility.java
@@ -2,6 +2,9 @@ package com.TestNGDemos;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 public class RediffUtility {
@@ -12,18 +15,31 @@ public class RediffUtility {
 	 * 3. Pass the driver object from client class to utility class via Constructor
 	 */
 	
+	/*
+	 * Page Factory
+	 * - Page factory is sub set of Page Object Model
+	 * - It is the collection of WebElements on the page
+	 * - It acts like object repository
+	 */
+	
 	WebDriver driver;
+	
+	@FindBy(xpath = "//input[@placeholder='Enter your full name']")WebElement fullName;
+	@FindBy(css = "input[placeholder=\"Enter Rediffmail ID\"]")WebElement rediffId;
 	
 	public RediffUtility(WebDriver d) {
 		this.driver = d;
+		PageFactory.initElements(driver, this);
 	}
 	
 	public void enterFullName(String fn) {
-		driver.findElement(By.xpath("//input[@placeholder='Enter your full name']")).sendKeys(fn);
+		//driver.findElement(By.xpath("//input[@placeholder='Enter your full name']")).sendKeys(fn);
+		fullName.sendKeys(fn);
 	}
 	
 	public void enterRediffId(String rid) {
-		driver.findElement(By.cssSelector("input[placeholder=\"Enter Rediffmail ID\"]")).sendKeys(rid);
+		//driver.findElement(By.cssSelector("input[placeholder=\"Enter Rediffmail ID\"]")).sendKeys(rid);
+		rediffId.sendKeys(rid);
 	}
 	
 	public void enterPassword(String ps) {
@@ -42,6 +58,14 @@ public class RediffUtility {
 			driver.findElement(By.xpath("//input[@value='m']")).click();
 		else
 			driver.findElement(By.xpath("//input[@value='f']")).click();
+	}
+	
+	public String checkAvailablity() throws InterruptedException {
+		driver.findElement(By.className("btn_checkavail")).click();
+		Thread.sleep(2000);
+		String message = driver.findElement(By.id("check_availability")).getText();
+		System.out.println("Message: " + message);
+		return message;
 	}
 
 }
